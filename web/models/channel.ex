48d237abfeba240f6fb24dc925e1b42e10449f3f
Martin Karrer <support@bmalum.com>
2015-10-13 11:57:47 +0200

    Fixes #8

--- web/models/channel.ex
+++ web/models/channel.ex
@@ -30,5 +30,6 @@ defmodule Constructeev.Channel do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:slug)
   end
 end
