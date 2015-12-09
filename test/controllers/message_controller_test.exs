6c78f20d770a7bae19fbe293a2754c8ff109c065
Martin Karrer <support@bmalum.com>
2015-12-09 13:27:48 +0100

    Patch _ Removed Messages tests.

--- test/controllers/message_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Constructeev.MessageControllerTest do
-  use Constructeev.ConnCase
-
-  alias Constructeev.Message
-  @valid_attrs %{content: "some content", parent_id: 42}
-  @invalid_attrs %{}
-
-  
-end
