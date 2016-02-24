2536b1f43e595d375f54b430b9973076642291c2
Martin Karrer <support@bmalum.com>
2016-02-24 06:26:11 +0100

    Routes added

--- web/router.ex
+++ web/router.ex
@@ -16,17 +16,29 @@ defmodule Constructeev.Router do
 
   scope "/", Constructeev do
     pipe_through :browser # Use the default browser stack
-
+    resources "/feedback_properties", FeedbackPropertyController, except: [:new, :edit]
+    resources "/channel_properties", ChannelPropertyController, except: [:new, :edit]
     get "/", PageController, :index
   end
 
   scope "/api", Constructeev do
      pipe_through :api
 
+     scope "/admin/" do
+      resources "/feedback_properties", FeedbackPropertyController
+      get "_debug", FeedbackPropertyController, :index
+      get "_read", AdminController, :read
+      get "_fav", AdminController, :fav
+      get "_display", AdminController, :display
+      get "_setstart", AdminController, :starttime
+      get "_setstop", AdminController, :stoptime
+     end
+
      resources "channels", ChannelController do
       get "_like", ChannelController, :like
       resources "feedbacks", FeedbackController do
         get "_like", FeedbackController, :like
+        get "_dislike", FeedbackController, :dislike
         get "_children", FeedbackController, :children 
       end
     end
@@ -34,7 +46,6 @@ defmodule Constructeev.Router do
      post "/sessions", SessionController, :create
      delete "/sessions", SessionController, :delete 
      get "/channel/_search", ChannelController, :search
-     #get "/feedback/_children", FeedbackController, :children
   end
 
 end
